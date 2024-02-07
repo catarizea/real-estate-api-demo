5cd1755f239dd9d0f91a22786120a14dcd559ce0
Catalin Rizea <catalinrizea1@gmail.com>
2024-02-08 00:12:04 +0200

    export codegen fields objects for conversion to query builder

---
+++ src/validators/codegen/index.ts
@@ -0,0 +1,15 @@
+import * as community from './list/community';
+import * as floorPlan from './list/floorPlan';
+import * as media from './list/media';
+import * as mediaType from './list/mediaType';
+import * as property from './list/property';
+
+export const floorPlanFields = floorPlan.fields;
+
+export const communityFields = community.fields;
+
+export const mediaFields = media.fields;
+
+export const mediaTypeFields = mediaType.fields;
+
+export const propertyFields = property.fields;
